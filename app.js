f5f858586c5ddcc66ed1b4caa5a1c0914a4ef9b8
Ali <aliabduh.2020@gmail.com>
2022-02-17 13:42:56 +0300

    add 404

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/', function(req, res) {
 res.sendfile(__dirname + '/index.html');
 });
 
+app.get('/index.html', function(req, res) {
+    res.sendfile(__dirname + '/');
+    });
+
 app.get('/blog', function(req, res) {
     res.sendfile(__dirname + '/index.html');
     });
@@ -24,23 +28,46 @@ app.get('/login', function(req, res) {
 res.sendfile(__dirname + '/login.html');
 });
 
-app.get('/admin', function(req, res) {
-    res.sendfile(__dirname + '/admin.html');
+app.get('/login.html', function(req, res) {
+    res.sendfile(__dirname + '/login');
     });
+
+app.get('/admin', function(req, res) {
+    res.sendfile(__dirname + '/login.html');
+    });
+
+    app.get('/about', function(req, res) {
+        res.sendfile(__dirname + '/about.html');
+        });
+
+        app.get('/about.html', function(req, res) {
+            res.sendfile(__dirname + '/about');
+            });
+
+        app.get('/contact', function(req, res) {
+            res.sendfile(__dirname + '/contact.html');
+            });
+
+            app.get('/contact.html', function(req, res) {
+                res.sendfile(__dirname + '/contact');
+                });
 
     
 app.get('*', function(req, res) {
     res.sendfile(__dirname + '/404.html');
     });
 
+    app.get('/404.html', function(req, res) {
+        res.sendfile(__dirname + '/404');
+        });
+
      
         app.get('/admin', function(req, res) {
             var roleName = 'admin';
-            if(req.query.hasOwnProperty('role')&&(req.query.role == roleName))
+            if( req.query.hasOwnProperty('role') && (req.query.role == roleName) )
             res.end(`welcome${roleName}`);
             else
             res.sendfile(__dirname + '/login.html');
-
             });
   
 
